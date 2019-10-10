29594436d28a05d335a1fdd01473296dabb42ae3
Laurents Meyer <laucomm@gmail.com>
2019-10-10 03:01:42 +0200

    Cannot reproduce https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/issues/863

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Issue863.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IndexModel(ILogger<IndexModel> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public async void OnGet()
+        {
+            await TestIssue863();
+        }
+
+        private async Task TestIssue863()
+        {
+            var users = _userManager.Users.ToArray();
+            var roles = await _userManager.GetRolesAsync(users.First());
+
+            foreach (var role in roles)
+            {
+                Debug.WriteLine(role);
+            }
+        }
+    }
+}
